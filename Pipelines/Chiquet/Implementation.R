b34019572eaa6420f6b150edc78651136e1ea1f3
Syed Umair Aziz <sua474@mail.usask.ca>
2021-05-26 16:38:59 -0600

    Updated the Chiquet Implementation
    1) The script can now take another argument as parameter specifying the output directory of the execution.
    2) Output directory will be create at the specified location (argument-2) if it was not already there.
    3) Enhanced the naming convention.

--- Pipelines/Chiquet/Implementation.R
+++ Pipelines/Chiquet/Implementation.R
@@ -1,11 +1,25 @@
 library(PLNmodels)
 library(ggplot2)
+library(stringr)
+
 rm(list = ls())
+args = commandArgs(trailingOnly=TRUE)
+
+
+get_file_name<-function(path_to_dir)
+{
+  fn <- str_split(path_to_dir,"/",simplify = FALSE)
+  fn <- fn[[1]][lengths(fn)]
+  fn <- str_split(fn, ".csv", simplify = FALSE)
+  return (str_trim(fn[[1]][1]))
+}
+
+
 ## Load the data: absolute abundances and covariates
-abundance <- read.csv("~/Dropbox/Cyclotron Research/Carbon_Oxidation_and_Fixation_Study/Metagenome/SteveM/Network/abu.species.glom1.csv", row.names = 2)[-1]
+abundance <- read.csv(args[1], row.names = 2)[-1]
 ## Prepare the dataset
 abundance = t(abundance)  # Transpose to have the samples as rows
-covariates <- read.csv("~/Dropbox/Cyclotron Research/Carbon_Oxidation_and_Fixation_Study/Metagenome/SteveM/Network/Transformed_Covariates.csv",row.names = 1)
+covariates <- read.csv("/u1/sua474/Dataset/Desert/Transformed_Covariates.csv",row.names = 1)
 identical(rownames(abundance), rownames(covariates))        # FALSE; there are spaces in the covariate row names
 rownames(covariates) <- gsub(" ","", rownames(covariates))  # Replace the spaces
 abundance <- abundance[rownames(covariates),]               # Sort abundances by row names in covariates to make row names match
@@ -17,5 +31,11 @@ model_StARS <- getBestModel(network_models, "StARS")                      # Netw
 plot(model_StARS, output = "igraph")                                      # Default plotting
 model_StARS.adj <- as.matrix(plot(model_StARS, output = "corrplot"))      # Adjacency matrix
 model_StARS.deg <- rowSums(model_StARS.adj != 0)                          # Node degrees
-write.csv(model_StARS.adj, "~/Dropbox/Cyclotron Research/Carbon_Oxidation_and_Fixation_Study/Metagenome/SteveM/Network/model_StARS.adj.csv")
-write.csv(model_StARS.deg, "~/Dropbox/Cyclotron Research/Carbon_Oxidation_and_Fixation_Study/Metagenome/SteveM/Network/model_StARS.deg.csv")
+
+dir.create(args[2])                                         # Creates the directory if not there
+
+adj_matrix_name = paste0("StARS_Adjacency_Matrix_",get_file_name(args[1]),".csv")     #Naming the output file
+deg_matrix_name = paste0("StARS_Degree_Distribution_",get_file_name(args[1]),".csv")  #Naming the output file
+
+write.csv(model_StARS.adj, paste0(args[2],adj_matrix_name)) # Writing the output
+write.csv(model_StARS.deg, paste0(args[2],deg_matrix_name)) # Writing the output
