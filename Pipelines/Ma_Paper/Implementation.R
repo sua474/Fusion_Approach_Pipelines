6e2f06face2e2607ba5a7d3e4ddcc4658ae793e7
Syed Umair Aziz <sua474@mail.usask.ca>
2021-05-27 13:51:13 -0600

    Updated Ma Paper Pipeline
    Added a second parameter which will create the output directory if not already there.

--- Pipelines/Ma_Paper/Implementation.R
+++ Pipelines/Ma_Paper/Implementation.R
@@ -1,21 +1,25 @@
-######################## Installtion Steps ################################
-#devtools::install_github("drjingma/metaMint")
-#library(metaMint)
-#vignette("metaMint")
-####################### Library Import ####################################
 library(metaMint)
 library(dplyr)
 library(GGally)
+library(stringr)
+
 args = commandArgs(trailingOnly=TRUE)
-########################## Code Test ######################################
-#data(BV, package = "metaMint")
-#print(t(OTUs))
-#OTUs_mclr <- mclr(t(OTUs))
-#print(OTUs_mclr)
-############################################################################
+
+dir.create(args[2]) #Craetes the output dir
+
+get_file_name<-function(path_to_dir)
+{
+  fn <- str_split(path_to_dir,"/",simplify = FALSE)
+  fn <- fn[[1]][lengths(fn)]
+  fn <- str_split(fn, ".csv", simplify = FALSE)
+  return (str_trim(fn[[1]][1]))
+}
+
 ######################### Processing Desert Dataset ########################
 dataset <- read.csv(file = args[1],row.names=1,header=TRUE)
 mclr_dataset <- mclr(t(dataset))
 pcorr <- cggm.pcorr(mclr_dataset,c(1.5,0.4,0.3,0.2,0.1,0.05),'glasso')
-print(pcorr$icov)
+adjacency_matrix <- as.data.frame(pcorr$icov)   #Extracts the Adjacency Matrix
 
+#Writes Output to the file
+write.csv(adjacency_matrix,paste0(args[2],"Adjacency_Matrix_",get_file_name(args[1]),".csv"),row.names = FALSE) 
