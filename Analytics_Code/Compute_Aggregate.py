dde2c5cdee3cbb807278a1276972eac5c19a86eb
Syed Umair Aziz <sua474@mail.usask.ca>
2021-09-10 13:11:06 -0600

    Updated Aggregate Pipeline
    This module now has two function:
    1) To compute aggregate of algorithm output files
    2) To compute aggregate of ground truth files

--- Analytics_Code/Compute_Aggregate.py
+++ Analytics_Code/Compute_Aggregate.py
@@ -4,7 +4,7 @@ from os.path import isfile, join
 from os import listdir
 import copy as c
 
-def compute_aggregate(traversal_path):
+def compute_aggregate_output(traversal_path):
 
     aggregate_dataset = pd.DataFrame()
     folder_count = 0
@@ -25,8 +25,28 @@ def compute_aggregate(traversal_path):
 
     return aggregate_dataset/folder_count
 
+def compute_aggregate_ground_truth(traversal_path):
+
+    aggregate_dataset = pd.DataFrame()
+    file_count = 0
+    for root, subFolders, files in os.walk(traversal_path):
+        for f in files:
+            file_count+=1
+            file_path = '{}/{}'.format(root,f)
+            print(file_path)
+            data = pd.read_csv(file_path)
+            if(aggregate_dataset.empty):
+                aggregate_dataset = c.copy(data)
+            else:
+                for i in range(0,data.shape[0]):
+                    for j in range(0,data.shape[1]):
+                        aggregate_dataset.iloc[i,j] = aggregate_dataset.iloc[i,j] + data.iloc[i,j]
+
+    print('Total Files Processed: {}'.format(file_count))
+    return aggregate_dataset/file_count
+
 if __name__ == '__main__':
 
-    output_folder = "Aggregated_Files/Ma_10.csv"
-    aggregated_dataset = compute_aggregate('/Users/sua474/Desktop/Work/Development/Fusion_Approach_Pipelines/Results/Ma_10/')
-    aggregated_dataset.to_csv(output_folder,index=False)
+    output_file = "I:/Research_Technician/Development/Fusion_Approach_Pipelines/Results/Aggregated_Files/Ground_Truth_100T_10IT.csv"
+    aggregated_dataset = compute_aggregate_ground_truth('I:/Research_Technician/Dataset/Chiquet/100_Taxa/Ground_Truth_10/')
+    aggregated_dataset.to_csv(output_file,index=False)
