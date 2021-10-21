cd52030666dd1d15d8adf2b8f27a18657456fc4b
Syed Umair Aziz <sua474@mail.usask.ca>
2021-10-21 12:50:16 -0600

    Batch Execution Script
    Added Batch Execution Script for Correlation and Spring algorithm

--- Pipelines/Correlation/Execute_Correlation.py
+++ Pipelines/Correlation/Execute_Correlation.py
@@ -4,13 +4,20 @@ import os
 
 #subprocess.run(["conda","activate","plant_counting"])
 execution_file = "pipeline_original.py"
-input_path = "/u2/sua474/Dataset/Chiquet/Taxa_10/Chiquet_10/"
-output_path = "/u2/sua474/Fusion_Approach_Pipelines/Output/Taxa_10/IT_10/Correlation/"
+input_path = "/u2/sua474/Dataset/Chiquet/"
+output_path = "/u2/sua474/Fusion_Approach_Pipelines/Output/"
 filename = "Chiquet_"
 
-for i in range(1,2):
-    read_loc = "{}{}{}.csv".format(input_path,filename,str(i))
-    write_loc = "{}{}{}/".format(output_path,filename,str(i))
-    print("Processing Input File: "+filename+str(i))
-    subprocess.run(["python",execution_file,"-f1",read_loc,"-m","graph_centrality","-e","kl_divergence","-min","0" ,"-c","add_one", "-st", "10" ,"-si" ,"10" ,"-cent" ,"degree" ,"-th" ,"0.2", "-cor", "pearson" ,"-cp" ,"both","-r",write_loc])
+for taxa in ["Taxa_30"]:
+    for internal_thresold in ["10","50","100"]:
+        for i in range(1,101):
+            
+            read_path = '{}{}/Chiquet_{}/'.format(input_path,taxa,internal_thresold)
+            write_path = '{}{}/IT_{}/Correlation/'.format(output_path,taxa,internal_thresold)
+        
+            read_loc = "{}{}{}.csv".format(read_path,filename,str(i))
+            write_loc = "{}{}{}/".format(write_path,filename,str(i))
+            
+            print("Processing Input File: "+filename+str(i))
+            subprocess.run(["python",execution_file,"-f1",read_loc,"-m","graph_centrality","-e","kl_divergence","-min","0" ,"-c","add_one", "-st", "10" ,"-si" ,"10" ,"-cent" ,"degree" ,"-th" ,"0.5", "-cor", "pearson" ,"-cp" ,"both","-r",write_loc])
 
--- Pipelines/Spring/Execute_Spring.py
+++ Pipelines/Spring/Execute_Spring.py
@@ -4,16 +4,22 @@ import os
 
 #subprocess.run(["conda","activate","plant_counting"])
 execution_file = "Implementation.R"
-input_path = "/u2/sua474/Dataset/Chiquet/Taxa_10/Chiquet_10/"
-output_path = "/u2/sua474/Fusion_Approach_Pipelines/Output/Taxa_10/IT_10/Spring/"
+input_path = "/u2/sua474/Dataset/Chiquet/"
+output_path = "/u2/sua474/Fusion_Approach_Pipelines/Output/"
 filename = "Chiquet_"
 
 #Params
 nlambda = 15
 
-for i in range(1,2):
-    read_loc = "{}{}{}.csv".format(input_path,filename,str(i))
-    write_loc = "{}{}{}/".format(output_path,filename,str(i))
-    print("Processing Input File: "+filename+str(i))
-    subprocess.run(["Rscript",execution_file,read_loc,write_loc,str(nlambda)])
+for taxa in ["Taxa_30"]:
+    for internal_thresold in ["10","50","100"]:
+        for i in range(1,101):
+            read_path = '{}{}/Chiquet_{}/'.format(input_path,taxa,internal_thresold)
+            write_path = '{}{}/IT_{}/Spring/'.format(output_path,taxa,internal_thresold)
+        
+            read_loc = "{}{}{}.csv".format(read_path,filename,str(i))
+            write_loc = "{}{}{}/".format(write_path,filename,str(i))
+        
+            print("Processing Input File: "+filename+str(i)+" Taxa: "+str(taxa)+" IT: "+str(internal_thresold)+"\n")
+            subprocess.run(["Rscript",execution_file,read_loc,write_loc,str(nlambda)])
 
